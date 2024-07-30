36522f9c01ff93a56e14af50ec8be74c2ca3581a
MuhammadSaeedSaeedyan <154613522+MuhammadSaeedSaeedyan@users.noreply.github.com>
2024-07-30 08:19:51 -0700

    Update shoe.js

--- zizi Shoe stores/src/models/shoe.js
+++ zizi Shoe stores/src/models/shoe.js
@@ -17,33 +17,6 @@ class Shoe {
         this.SpecialFeaturesOfShoes = SpecialFeaturesOfShoes;
         this.ShoeSizes = ShoeSizes;
     }
-
-    // متدی برای اضافه کردن تعداد کفش برای یک سایز خاص
-    // addSize(size, count) {
-    //     if (this.ShoeSizes[size]) {
-    //         this.ShoeSizes[size] += count;
-    //     } else {
-    //         this.ShoeSizes[size] = count;
-    //     }
-    // }
-
-    // متدی برای گرفتن تعداد کفش‌ها برای یک سایز خاص
-    // getSizeCount(size) {
-    //     return this.ShoeSizes[size] || 0;
-    // }
-
-    // متدی برای کم کردن تعداد کفش برای یک سایز خاص
-    // removeSize(size, count) {
-    //     if (this.ShoeSizes[size]) {
-    //         if (this.ShoeSizes[size] >= count) {
-    //             this.ShoeSizes[size] -= count;
-    //         } else {
-    //             console.error(`تعداد کفش‌ها برای سایز ${size} کمتر از مقدار مورد نظر است`);
-    //         }
-    //     } else {
-    //         console.error(`سایز ${size} وجود ندارد`);
-    //     }
-    // }
 }
 
 
