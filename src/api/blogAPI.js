b391bd0d1adef96d40fff0c4759bca81a3f74d4d
MuhammadSaeedSaeedyan <154613522+MuhammadSaeedSaeedyan@users.noreply.github.com>
2024-11-08 11:22:14 -0800

    Delete src/api directory

--- src/api/blogAPI.js
+++
@@ -1,8 +0,0 @@
-class BlogApi extends BaseApi {
-    getAll = async (onsuccess) => {
-        this.getData("blog", onsuccess);
-    }
-    getById = async (id, onsuccess) => {
-        this.getData(`blog/${id}`, onsuccess);
-    }
-}
--- src/api/categoryAPI.js
+++
@@ -1,8 +0,0 @@
-class CategoryApi extends BaseApi {
-    getAll = async (onsuccess) => {
-        this.getData("productCategory", onsuccess);
-    }
-    getById = async (id, onsuccess) => {
-        this.getData(`productCategory/${id}`, onsuccess);
-    }
-}
