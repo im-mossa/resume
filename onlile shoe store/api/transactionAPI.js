a0ec344011f6e9ae3073b4e11bd0ea0a4244d91e
MuhammadSaeedSaeedyan <154613522+MuhammadSaeedSaeedyan@users.noreply.github.com>
2024-11-08 11:23:22 -0800

    Delete onlile shoe store/api directory

--- onlile shoe store/api/transactionAPI.js
+++
@@ -1,6 +0,0 @@
-class TransactionApi extends BaseApi {
-    goToPayment = async (data, onsuccess) => {
-        let token = getCookie("token");
-        this.postDataWithToken("trx/gotoPayment", data, token, onsuccess);
-    }
-}
