03665172e69b02b93c5871b9aadbe0307ebef31f
mohammad saeed saeedian <ssaeeedyan@gmail.com>
2024-11-20 08:52:45 -0800

    delete windmill-dashboard file and update contentAPI.js

--- online shoe store/src/api/contentAPI.js
+++ online shoe store/src/api/contentAPI.js
@@ -2,8 +2,4 @@ class AboutApi extends BaseApi {
     getAll = async (onsuccess) => {
         this.getData("content", onsuccess);
     }
-    // Because there is only one item, this part of the code was not used
-    // getData = async (title, onsuccess) => {
-    //     this.getData(`content/${title}`, onsuccess);
-    // }
 }
