fed4e020808a93c9438bd924d5468b3e797b372d
mohammad saeed saeedian <ssaeeedyan@gmail.com>
2024-11-26 03:53:03 -0800

    rename nasa to Space knowledge for all file and folder. update css file for travel and Space knowledge. update index.html for travel and Space knowledge

--- nasa/src/js/logic.js
+++ nasa/src/js/logic.js
@@ -1,32 +1,44 @@
-function loadFile() {
-    const url = new Url().getApiUrl();
-    const handler = new ApiHandler();
-    const post = document.getElementById("post");
-    const holder = document.createElement("div");
+window.onload = () => {
+    let getData = sessionStorage.getItem("data");
+    if (!getData) {
+        loadFile();
+    } else {
+        loadPage(JSON.parse(getData));
+    }
+    function loadFile() {
+        const url = new Url().getApiUrl();
+        const handler = new ApiHandler();
 
-    function removeDoubleQuotes(url) {
-        return url ? url.replace(/"/g, '') : ''; // حذف تمام دابل کوتیشن‌ها یا برگرداندن رشته خالی در صورت undefined بودن
+        handler.getData(url, function (data) {
+            sessionStorage.setItem("data", JSON.stringify(data));
+            loadPage(data);
+        }, function (error) {
+            console.log(error);
+        });
     }
 
-    handler.getData(url, function (data) {
+    function loadPage(data) {
+        const post = document.getElementById("post");
+        const holder = document.createElement("div");
+        holder.className = "holder";
+
         const template = `
-            <div class="template">
-                <h3>__TITLE__</h3>
-                <img src="__IMG__" alt="This is an image">
-                <p>__EXPLANATION__</p>
-            </div>`;
+                <div class="template">
+                    <h3>__TITLE__</h3>
+                    <img src="__IMG__" alt="This is an image">
+                    <p>__EXPLANATION__</p>
+                </div>`;
+
+        function removeDoubleQuotes(url) {
+            return url ? url.replace(/"/g, '') : ''; // حذف تمام دابل کوتیشن‌ها یا برگرداندن رشته خالی در صورت undefined بودن
+        }
 
         data.forEach(element => {
             let currentItemTemplate = template;
             let currentItem = element;
 
-            // بررسی مقدار currentItem.img برای جلوگیری از خطا
-            // debugger;
-            // const img = currentItem.img ? removeDoubleQuotes(currentItem.url) : 'https://apod.nasa.gov/apod/image/1410/amsiss_sts134_1080.jpg'; // جایگزینی تصویر پیش‌فرض در صورت undefined بودن
-            // debugger;
-
             currentItemTemplate = currentItemTemplate.replace("__TITLE__", currentItem.title || "No Title");
-            currentItemTemplate = currentItemTemplate.replace("__IMG__", removeDoubleQuotes(currentItem.url) || 'https://apod.nasa.gov/apod/image/1410/amsiss_sts134_1080.jpg');
+            currentItemTemplate = currentItemTemplate.replace("__IMG__", removeDoubleQuotes(currentItem.url) || 'https://apod.Space knowledge.gov/apod/image/1410/amsiss_sts134_1080.jpg');
             currentItemTemplate = currentItemTemplate.replace("__EXPLANATION__", currentItem.explanation || "No explanation available.");
 
             // ایجاد یک div جدید برای هر آیتم
@@ -40,16 +52,7 @@ function loadFile() {
 
         // اضافه کردن holder به post بعد از تکمیل حلقه
         post.appendChild(holder);
-
-        console.log(data);
-    }, function (error) {
-        console.log(error);
-    });
+    }
 }
 
-// window.onload = () => loadFile();
 
-
-// data.forEach(element => {
-//     console.log(element);
-// });
