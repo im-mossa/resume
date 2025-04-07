89e0f198c234a8a770446466b241f7b760d1e3cd
mohammad saeed saeedian <ssaeeedyan@gmail.com>
2025-04-07 14:03:43 -0700

    انجام تغییرات در ایندکس اصلی و نکست کانفیگ

--- travel-site/next.config.js
+++ travel-site/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-  };
-  
-  module.exports = nextConfig;
-  
+  output: 'export',
+  basePath: '/resume/travel-site',
+  assetPrefix: '/resume/travel-site/',
+};
+
+module.exports = nextConfig;
