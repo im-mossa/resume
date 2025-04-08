08e950f257d4da32c96988a38adb18a818636be9
mohammad saeed saeedian <ssaeeedyan@gmail.com>
2025-04-08 06:56:26 -0700

    انجام اصلاحات سوم روی travel-site

--- travel-site/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/resume/travel-site',
-  assetPrefix: '/resume/travel-site/',
-};
-
-module.exports = nextConfig;
