52941912a4fe31ff4070dfa336a9986d747b00b2
mohammad saeed saeedian <ssaeeedyan@gmail.com>
2025-04-30 13:50:48 -0700

    update vite.config

--- moody food/vite.config.ts
+++ moody food/vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/resume/moody food/',
+  base: '/resume/moody%20food/',
   server: {
     host: "::",
     port: 8080,
