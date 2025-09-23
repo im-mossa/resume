5466d2cde903ece705e26d91db458b669f3955a2
mohammad saeed saeedian <ssaeeedyan@gmail.com>
2025-09-23 04:41:26 -0700

    node.js practice part 5

---
+++ express.js/p1/logger.js
@@ -0,0 +1,20 @@
+// logger.js
+const pino = require('pino');
+
+const logger = pino({
+    level: process.env.LOG_LEVEL || 'info',
+    transport: {
+        targets: [
+            {
+                target: 'pino-pretty', // برای خوانایی در توسعه
+                options: {
+                    colorize: true,
+                    translateTime: 'SYS:standard',
+                    ignore: 'pid,hostname'
+                }
+            }
+        ]
+    }
+});
+
+module.exports = logger;
