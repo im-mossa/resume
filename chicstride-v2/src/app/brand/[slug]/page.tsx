c6952bb00d54279350b4296883a77ad720b8bd66
mohammad saeed saeedian <ssaeeedyan@gmail.com>
2025-11-18 11:15:48 -0800

    resolve format

--- chicstride-v2/src/app/brand/[slug]/page.tsx
+++ chicstride-v2/src/app/brand/[slug]/page.tsx
@@ -1,8 +1,8 @@
 // src/app/brand/[slug]/page.tsx
 import React from 'react';
-import { apiClient } from "../../../lib/api/client";
-import { Brand } from "../../../entities/brand";
-import { Product } from "../../../entities/product";
+import { apiClient } from '../../../lib/api/client';
+import { Brand } from '../../../entities/brand';
+import { Product } from '../../../entities/product';
 import { buildPublicImageUrl } from '../../../lib/utils/images';
 import BrandHeader from '../../../ui/components/brand/BrandHeader';
 import BrandProductGrid from '../../../ui/components/brand/BrandProductGrid';
